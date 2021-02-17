26fc878f56a651d6cecbef2e80479f71394a2dd8
bad64 <lvincent@alara-expertise.fr>
2021-02-17 14:37:48 +0100

    Reworked the DLL to return request headers

--- src/lib.rs
+++ src/lib.rs
@@ -17,19 +17,35 @@ use std::net::TcpStream;
 use dns_lookup::lookup_host;
 
 #[no_mangle]
-pub extern "C" fn request(method: *const c_char, hostent: *const c_char, content: *const c_char) -> *mut c_char {
+#[repr(C)]
+pub struct Request {
+    request: *mut i8,
+    response: *mut i8,
+}
+
+#[no_mangle]
+pub extern "C" fn ffi_request(method: *const c_char, hostent: *const c_char, content: *const c_char, agent: *const c_char) -> Request {
     let r_method = unsafe { CStr::from_ptr(method).to_str().unwrap() };
     let r_hostent = unsafe { CStr::from_ptr(hostent).to_str().unwrap() };
     let r_content = unsafe { CStr::from_ptr(content).to_str().unwrap() };
+    let r_agent = unsafe { CStr::from_ptr(agent).to_str().unwrap() };
+
+    let mut r = Request {
+        request: CString::new("").unwrap().into_raw(),
+        response: CString::new("").unwrap().into_raw()
+    };
 
     // Is http:// prefix present ?
     let mut offset = 0;
     if r_hostent.contains("http://") {
         offset = 7;
     }
+    else if r_hostent.contains("https://") {
+        offset = 8;
+    }
 
     // Split host, port, and resource
-    let mut r_host: String;
+    let r_host: String;
     let mut r_port = 80;
     let mut r_resource = "/".to_string();
     
@@ -55,6 +71,11 @@ pub extern "C" fn request(method: *const c_char, hostent: *const c_char, content
 
     // Build the request string
     let mut req = format!("{} {} HTTP/1.1\r\nHost: {}\r\n", r_method, r_resource, r_host);
+    if r_agent != "" {
+        req.push_str("User-Agent: ");
+        req.push_str(&r_agent);
+        req.push_str("\r\n");
+    }
     if r_method == "POST" {
         req.push_str("Content-type: application/json\r\nContent-length: ");
         req.push_str(&r_content.len().to_string());
@@ -66,20 +87,47 @@ pub extern "C" fn request(method: *const c_char, hostent: *const c_char, content
         req.push_str(r_content);
     }
 
+    r.request = CString::new(req.clone()).unwrap().into_raw();
+
     // Establish connection
     let ips: Vec<std::net::IpAddr> = lookup_host(&r_host).unwrap();
 
     let addr = SocketAddr::new(ips[0], r_port);
-    let mut sock = TcpStream::connect_timeout(&addr, Duration::new(30, 0)).unwrap();
+    let sock = TcpStream::connect_timeout(&addr, Duration::new(30, 0));
 
-    // Send data
-    sock.write(req.as_bytes()).expect("Could not write to socket");
+    match sock {
+        Ok(mut s) => {
+            // Send data
+            s.write(req.as_bytes()).expect("Could not write to socket");
 
-    // Receive data
-    let mut resp = String::new();
-    sock.read_to_string(&mut resp).expect("Could not read from socket");
+            // Receive data
+            let mut resp = String::new();
+            s.read_to_string(&mut resp).expect("Could not read from socket");
 
-    return CString::new(resp).unwrap().into_raw();
+            r.response = CString::new(resp).unwrap().into_raw();
+
+            return r;
+        },
+        Err(e) => {
+            let mut error = String::new();
+            error.push_str("{");
+            error.push_str("\"method\": \"");
+            error.push_str(&r_method);
+            error.push_str("\",");
+            error.push_str("\"hostent\": \"");
+            error.push_str(&r_host);
+            error.push_str(":");
+            error.push_str(&r_port.to_string());
+            error.push_str("\",");
+            error.push_str("\"error\": \"");
+            error.push_str(&e.to_string());
+            error.push_str("\"}");
+            
+            r.response = CString::new(error).unwrap().into_raw();
+
+            return r;
+        },
+    }
 }
 
 #[no_mangle]
